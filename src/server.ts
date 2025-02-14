1df9dc41fa0659891fbe42df56fb406a57709f26
Vicentecarvalho-3600 <vicentecarvalho.cooknet@gmail.com>
2025-02-14 02:01:06 -0300

    adiciondo cors ao projeto

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import express, { Request, Response, NextFunction } from "express";
 import "express-async-errors";
+import cors from "cors";
 import { routes } from "./routes/routes";
 import "dotenv/config";
 import { AppError } from "./errors/AppError";
 const app = express();
 app.use(express.json());
+app.use(cors());
 app.use(routes);
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction): any => {
