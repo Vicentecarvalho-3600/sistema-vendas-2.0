87fc4d71821a943801980d78fe4256ade288c7c8
Vicentecarvalho-3600 <vicentecarvalho.cooknet@gmail.com>
2025-02-17 15:07:30 -0300

    adicionado rotas post/login e get/me em userRoutes

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,10 +1,21 @@
-import { NextFunction, Router } from "express";
+import { Router } from "express";
 import { createUserController } from "../instances/user/createUser";
+import { authUserController } from "../instances/user/authUser";
+import { detailsUserController } from "../instances/user/detailsUser";
+import { isAuthenticated } from "../middlewares/isAuthenticated";
 
 const userRoutes = Router();
 
-userRoutes.post("/", async (req, res, next: NextFunction) => {
+userRoutes.post("/", (req, res, next) => {
   createUserController.handle(req, res, next);
+});
+
+userRoutes.post("/login", (req, res, next) => {
+  authUserController.handle(req, res, next);
+});
+
+userRoutes.get("/me", isAuthenticated, (req, res, next) => {
+  detailsUserController.handle(req, res, next);
 });
 
 export { userRoutes };
