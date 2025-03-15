5b8751498aab5fedc1e6e39ee2d6cec2695b008d
Vicentecarvalho-3600 <vicentecarvalho.cooknet@gmail.com>
2025-03-15 03:26:46 -0300

    adicioando configuração multer para updade de imagens

--- src/server.ts
+++ src/server.ts
@@ -3,10 +3,14 @@ import cors from "cors";
 import { routes } from "./routes/routes";
 import "dotenv/config";
 import { AppError } from "./errors/AppError";
+import path from "node:path";
+
 const app = express();
 app.use(express.json());
 app.use(cors());
+
 app.use(routes);
+app.use("/files", express.static(path.resolve(__dirname, "..", "tmp")));
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction): any => {
   if (err instanceof AppError) {
