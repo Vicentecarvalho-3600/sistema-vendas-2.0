31e7649c7750e72318af64c85ae400c0d75acede
Vicentecarvalho-3600 <vicentecarvalho.cooknet@gmail.com>
2025-03-15 18:45:12 -0300

    adicionado router index orderRoutes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,11 +2,13 @@ import { Router } from "express";
 import { userRoutes } from "./user.routes";
 import { categoryRoutes } from "./category.routes";
 import { productRoutes } from "./product.routes";
+import { orderRoutes } from "./order.routes";
 
 const routes = Router();
 
 routes.use("/users", userRoutes);
 routes.use("/category", categoryRoutes);
 routes.use("/product", productRoutes);
+routes.use("/order", orderRoutes);
 
 export { routes };
