f4d83bb7e7bfbdba36c8e4a118a3194b50c41811
Vicentecarvalho-3600 <vicentecarvalho.cooknet@gmail.com>
2025-03-19 20:28:33 -0300

    adicionado a order.routes delete removeItem

--- src/routes/order.routes.ts
+++ src/routes/order.routes.ts
@@ -3,6 +3,7 @@ import { isAuthenticated } from "../middlewares/isAuthenticated";
 import { createOrderController } from "../instances/order/createOrder";
 import { removeOrderController } from "../instances/order/removeOrder";
 import { addItemController } from "../instances/order/addItem";
+import { removeItemController } from "../instances/order/removeItem";
 
 const orderRoutes = Router();
 
@@ -16,6 +17,10 @@ orderRoutes.delete("/", isAuthenticated, (req, res, next) => {
 
 orderRoutes.post("/add", isAuthenticated, (req, res, next) => {
   addItemController.handle(req, res, next);
+});
+
+orderRoutes.delete("/remove", isAuthenticated, (req, res, next) => {
+  removeItemController.handle(req, res, next);
 });
 
 export { orderRoutes };
