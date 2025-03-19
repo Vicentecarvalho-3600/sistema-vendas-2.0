6420ab3bdfc86a5bc4d5e15605f7581edbb33963
Vicentecarvalho-3600 <vicentecarvalho.cooknet@gmail.com>
2025-03-19 20:26:59 -0300

    criado class RemoveItemController

---
+++ src/controllers/order/RemoveItemController.ts
@@ -0,0 +1,17 @@
+import { NextFunction, Response, Request } from "express";
+import { RemoveItemService } from "../../services/order/RemoveItemService";
+
+class RemoveItemController {
+  constructor(private removeItemService: RemoveItemService) {}
+  async handle(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { itemId } = req.body;
+      const item = await this.removeItemService.execute({ itemId });
+      return res.json(item);
+    } catch (error) {
+      next(error);
+    }
+  }
+}
+
+export { RemoveItemController };
