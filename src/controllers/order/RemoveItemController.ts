41e6b02f0ea4a4a7c782ed9834eaad4216d0416e
Vicente Carvalho <vicentecarvalho.cooknet@gmail.com>
2025-03-26 01:23:45 -0300

    fix class RemoveItemController

--- src/controllers/order/RemoveItemController.ts
+++ src/controllers/order/RemoveItemController.ts
@@ -5,7 +5,7 @@ class RemoveItemController {
   constructor(private removeItemService: RemoveItemService) {}
   async handle(req: Request, res: Response, next: NextFunction) {
     try {
-      const { itemId } = req.body;
+      const itemId = req.query.item_id as string;
       const item = await this.removeItemService.execute({ itemId });
       return res.json(item);
     } catch (error) {
